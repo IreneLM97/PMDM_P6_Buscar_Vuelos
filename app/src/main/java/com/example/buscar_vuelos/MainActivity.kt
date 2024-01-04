33c694418a1c5fc75bfd3d1cf6171222bf777a27
IreneLM97 <irenelm1997@gmail.com>
2024-01-04 13:06:43 +0100

    Creación proyecto

---
+++ app/src/main/java/com/example/buscar_vuelos/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.buscar_vuelos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.buscar_vuelos.ui.theme.PMDM_P6_Buscar_VuelosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PMDM_P6_Buscar_VuelosTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PMDM_P6_Buscar_VuelosTheme {
+        Greeting("Android")
+    }
+}
