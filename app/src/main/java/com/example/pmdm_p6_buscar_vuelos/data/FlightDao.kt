a84155f14c672cc90af692541fcdf64be4cb0179
IreneLM97 <irenelm1997@gmail.com>
2024-01-09 19:33:28 +0100

    Cambio en Dao para mostrar vuelos ordenados

--- app/src/main/java/com/example/pmdm_p6_buscar_vuelos/data/FlightDao.kt
+++ app/src/main/java/com/example/pmdm_p6_buscar_vuelos/data/FlightDao.kt
@@ -11,13 +11,13 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FlightDao {
-    @Query("SELECT * FROM airport WHERE iata_code = :query OR name LIKE '%' || :query || '%' ORDER BY passengers ASC")
+    @Query("SELECT * FROM airport WHERE iata_code = :query OR name LIKE '%' || :query || '%' ORDER BY passengers DESC")
     fun getAllAirports(query: String): Flow<List<Airport>>
 
     @Query("SELECT * FROM airport WHERE id = :id")
     fun getAirportById(id: Int): Flow<Airport>
 
-    @Query("SELECT * FROM favorite ORDER BY id ASC")
+    @Query("SELECT * FROM favorite ORDER BY id DESC")
     suspend fun getAllFavorites(): List<Favorite>
 
     @Query("SELECT * FROM favorite WHERE departure_code = :departureCode AND destination_code = :destinationCode")
